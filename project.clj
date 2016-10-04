690ce044558a062267941f87539ab374e27eae1f
blak3mill3r <blak3mill3r@gmail.com>
2016-10-04 16:46:25 +0000

    bump dependencies for Clojure and Scala

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-scalac "0.1.0"
+(defproject lein-scalac "0.1.2"
   :eval-in-leiningen true
-  :dependencies [[org.scala-lang/scala-compiler "2.9.1"]
+  :dependencies [[org.scala-lang/scala-compiler "2.11.8"]
                  [lancet "1.0.1"]])
--- test-project/project.clj
+++ test-project/project.clj
@@ -1,9 +1,9 @@
 (defproject test-project "0.1.0"
   :description "test project with some scala"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.scala-lang/scala-library "2.9.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.scala-lang/scala-library "2.11.8"]]
   :scala-source-path "scala"
-  :plugins [[lein-scalac "0.1.0"]]
+  :plugins [[lein-scalac "0.1.2"]]
   :prep-tasks ["scalac"]
   ;; for Leiningen 1.x you need this instead:
   ;; :hooks [leiningen.hooks.scalac]
